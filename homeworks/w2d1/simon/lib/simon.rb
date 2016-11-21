cce2ee690105ac7bd394eed98af3922e17d09f0c
Scott Mosher <scottmosher@Scotts-MacBook-Air.local>
2016-11-20 17:11:01 -0800

    finished practice test and w2d1 hw

---
+++ homeworks/w2d1/simon/lib/simon.rb
@@ -0,0 +1,53 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    take_turn until game_over
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+    @sequence_length + 1
+  end
+
+  def round_success_message
+    "Good Job!"
+  end
+
+  def game_over_message
+    p "You Lose!"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
